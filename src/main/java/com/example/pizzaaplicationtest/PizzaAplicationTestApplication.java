c6407fe0e5473ee8dca4285833686b07b96df6e6
Piotr Kwiecień <pitkwiecien@gmail.com>
2022-05-17 08:50:37 +0200

    Initial commit

---
+++ src/main/java/com/example/pizzaaplicationtest/PizzaAplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzaaplicationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaAplicationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaAplicationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pizzaaplicationtest/PizzaAplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzaaplicationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaAplicationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
