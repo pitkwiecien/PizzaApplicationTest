27aed46e8e4052b1cfd85f1e9282391cc0a274bc
Piotr Kwiecień <pitkwiecien@gmail.com>
2022-05-17 10:02:57 +0200

    Added Entity classes

--- src/main/java/com/example/pizzaaplicationtest/data/entity/pizza/PizzaEntity.java
+++ src/main/java/com/example/pizzaaplicationtest/data/entity/pizza/PizzaEntity.java
@@ -1,4 +1,45 @@
 package com.example.pizzaaplicationtest.data.entity.pizza;
 
+import com.example.pizzaaplicationtest.data.entity.size.SizeEntity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "pizzas")
 public class PizzaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "pizza", orphanRemoval = true)
+    private Set<SizeEntity> sizes;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<SizeEntity> getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(Set<SizeEntity> sizes) {
+        this.sizes = sizes;
+    }
 }
--- src/main/java/com/example/pizzaaplicationtest/data/entity/size/SizeEntity.java
+++ src/main/java/com/example/pizzaaplicationtest/data/entity/size/SizeEntity.java
@@ -1,4 +1,84 @@
 package com.example.pizzaaplicationtest.data.entity.size;
 
+import com.example.pizzaaplicationtest.data.entity.pizza.PizzaEntity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "sizes")
 public class SizeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "size_type")
+    private String sizeType;
+
+    @Column(name = "price_base")
+    private BigDecimal priceBase;
+
+    @Column(name = "pizza_id")
+    private Integer pizzaId;
+
+    //To przy potencjalnym przyszłym dodaniu OrderSizeEntity
+    /*
+    @OneToMany(mappedBy = "size")
+    private Set<OrderSizeEntity> orderSizes;
+    */
+
+    @ManyToOne
+    @JoinColumn(name = "pizza_id", insertable = false, updatable = false)
+    private PizzaEntity pizza;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSizeType() {
+        return sizeType;
+    }
+
+    public void setSizeType(String sizeType) {
+        this.sizeType = sizeType;
+    }
+
+    public BigDecimal getPriceBase() {
+        return priceBase;
+    }
+
+    public void setPriceBase(BigDecimal priceBase) {
+        this.priceBase = priceBase;
+    }
+
+    public Integer getPizzaId() {
+        return pizzaId;
+    }
+
+    public void setPizzaId(Integer pizzaId) {
+        this.pizzaId = pizzaId;
+    }
+
+    /*
+    public Set<OrderSizeEntity> getOrderSizes() {
+        return orderSizes;
+    }
+
+    public void setOrderSizes(Set<OrderSizeEntity> orderSizes) {
+        this.orderSizes = orderSizes;
+    }
+    */
+
+    public PizzaEntity getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(PizzaEntity pizza) {
+        this.pizza = pizza;
+    }
 }
